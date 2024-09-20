2135f6d3c76e349a6b10f564567130e097c4c5b5
pratikMane07 <pratiksmane07@gmail.com>
2024-09-20 18:49:56 +0530

    add m1 by-pratik

--- BookMyShow/src/main/java/com/example/demo/BookMyShowApplication.java
+++ BookMyShow/src/main/java/com/example/demo/BookMyShowApplication.java
@@ -5,9 +5,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BookMyShowApplication {
+	
+	public void m1()
+	{
+		System.out.println("m1 method");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookMyShowApplication.class, args);
+		System.out.println("hello world");
 	}
 
 }
